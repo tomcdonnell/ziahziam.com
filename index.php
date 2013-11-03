ab5ba629cc08976b61932f966f094d379aba7541
Tom McDonnell <tomcdonnell@gmail.com>
2013-11-04 10:49:12 +1100

    Added placeholder for while site under construction.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'lib/tom/php/utils/UtilsHtml.php';
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Ziah Ziam</title>
+<?php
+UtilsHtml::echoHtmlScriptAndLinkTagsForJsAndCssFiles
+(
+   array('pages/common/css/style.css'),
+   array()
+);
+?>
+ </head>
+ <body>
+  <div id='main-content-div'>
+   <h1><img src='pages/common/images/ziah_ziam_banner.png' alt='Ziah Ziam'/></h1>
+<!--   <img src='pages/common/images/ziah_ziam_home_background.png' alt='Background'/>-->
+   <p>This site is under construction.</p>
+   <p>Direct all enquiries to <a href='http://facebook.com/luketylim'>Luke Tylim</a>.</p>
+  </div>
+ </body>
+</html>
